089363f0ee68b260d629d1c149697241170ed8ad
huchuchu <huchuchu.yoo@gmail.com>
2025-05-05 23:10:41 +0900

    동기 비동기 처리리

--- vue-til/src/components/SignupForm.vue
+++ vue-til/src/components/SignupForm.vue
@@ -13,7 +13,8 @@
 			<label for="nickname">nickname : </label>
 			<input type="text" id="nickname" v-model="nickname" />
 		</div>
-		<button type="submit">login</button>
+		<button type="submit">회원가입</button>
+		<p>{{ logMessage }}</p>
 	</form>
 </template>
 
@@ -27,17 +28,31 @@ export default {
 			username: '',
 			password: '',
 			nickname: '',
+			logMessage: '',
 		};
 	},
 	methods: {
-		submitForm() {
+		async submitForm() {
 			console.log('form 제출');
 			const userData = {
 				username: this.username,
 				password: this.password,
 				nickname: this.nickname,
 			};
-			registerUser(userData);
+			// const response = await registerUser(userData);
+			// console.log('response', response.data);
+
+			// destructuring
+			const { data } = await registerUser(userData);
+			console.log('data', data.username);
+			this.logMessage = `${data.username} 님이 가입되었습니다.`;
+
+			this.initForm();
+		},
+		initForm() {
+			this.username = '';
+			this.password = '';
+			this.nickname = '';
 		},
 	},
 };
--- vue-til/src/views/SignupPage.vue
+++ vue-til/src/views/SignupPage.vue
@@ -1,4 +1,5 @@
 <template>
+	<div>회원가입 페이지</div>
 	<SignupForm></SignupForm>
 </template>
 
