4c9669cd300095419ead438af53f1b68d5598744
huchuchu <huchuchu.yoo@gmail.com>
2025-05-06 10:45:13 +0900

    env 파일 dev prod 구분분

--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'http://localhost:3000/',
+	// baseURL: 'http://localhost:3000/',
+	baseURL: process.env.VUE_APP_API_URL,
 });
 
 function registerUser(userData) {
