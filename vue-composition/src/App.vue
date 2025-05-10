c9035f6026323538741a2807838a374f1289ad2a
huchuchu <huchuchu.yoo@gmail.com>
2025-05-10 23:09:31 +0900

    todo app

--- vue-composition/src/App.vue
+++ vue-composition/src/App.vue
@@ -1,42 +1,53 @@
 <template>
-	<div>
-		<p>{{ message }}</p>
-		<button @click="changeMessage">change</button>
-	</div>
+	<TodoHeader></TodoHeader>
+	<!-- <TodoInput @하위컴포넌트의 이벤트 이름="상위컴포넌트의 메서드 이름"></TodoInput> -->
+	<TodoInput @add="addTodoItem"></TodoInput>
+	<!-- <TodoList :프롭스이름="상위 컴포넌트의 데이터 이름"></TodoList> -->
+	<TodoList :todoItems="todoItems" @remove="removeTodoItem"></TodoList>
 </template>
 
 <script>
-import { useMessage } from './composable/useMessage';
+import { ref } from 'vue';
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
 
 export default {
-	setup() {
-		// [ver 3]
-		const { message, changeMessage } = useMessage();
-		return { message, changeMessage };
-
-		// [ver 2]
-		// data
-		// const message = ref('hello');
-
-		//methods
-		// function changeMessage(){
-		//   message.value = 'hi';
-		// }
-
-		// return {message, changeMessage}
+	components: {
+		TodoHeader,
+		TodoInput,
+		TodoList,
 	},
+	setup() {
+		//data
+		const todoItems = ref([]);
 
-	// [ver 1]
-	// data() {
-	//   return {
-	//     message: 'hello'
-	//   }
-	// },
-	// methods:{
-	//   changeMessage(){
-	//     this.message = 'hi';
-	//   }
-	// }
+		//method
+		function fetchTodos() {
+			const result = [];
+			for (let i = 0; i < localStorage.length; i++) {
+				const todoItem = localStorage.key(i);
+				// items.value.push(todoItem);
+				result.push(todoItem);
+			}
+
+			return result;
+		}
+
+		todoItems.value = fetchTodos();
+
+		function addTodoItem(todo) {
+			todoItems.value.push(todo);
+			localStorage.setItem(todo, todo);
+		}
+
+		function removeTodoItem(item, index) {
+			todoItems.value.splice(index, 1);
+			localStorage.removeItem(item);
+		}
+
+		return { todoItems, addTodoItem, removeTodoItem };
+	},
 };
 </script>
 
---
+++ vue-composition/src/components/TodoInput.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<input type="text" v-model="todoInput" />
+		<button @click="addTodo">add</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup(props, context) {
+		// data
+		const todoInput = ref('');
+
+		function addTodo() {
+			const todo = todoInput.value;
+			// localStorage.setItem(todo, todo);
+			// 상위 컴포넌트에서 바인딩되는 key : add
+			context.emit('add', todo);
+			clearTodo();
+		}
+
+		const clearTodo = () => (todoInput.value = '');
+
+		return { todoInput, addTodo, clearTodo };
+	},
+};
+</script>
+
+<style scoped></style>
