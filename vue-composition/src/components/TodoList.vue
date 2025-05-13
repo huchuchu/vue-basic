c8e88e03d1dde09f05ae52c0ee89c015281e46b5
huchuchu <huchuchu.yoo@gmail.com>
2025-05-13 20:33:18 +0900

    watch api

--- vue-composition/src/components/TodoList.vue
+++ vue-composition/src/components/TodoList.vue
@@ -8,6 +8,8 @@
 </template>
 
 <script>
+import { watch } from 'vue';
+
 export default {
 	props: ['todoItems'],
 	setup(props, context) {
@@ -15,6 +17,10 @@ export default {
 			context.emit('remove', item, index);
 		}
 
+		// 최대한 watch에 의존하지않길 추천(추적이 어려울수도있음)
+		watch(props.todoItems, newValue => {
+			console.log(newValue);
+		});
 		return { removeTodo };
 	},
 };
