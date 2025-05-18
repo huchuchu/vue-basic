85f8ab493217dbeffb9a4b1e2116c2e01f0168bb
huchuchu <huchuchu.yoo@gmail.com>
2025-05-18 22:58:23 +0900

    mainPage 목록보기

--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { setInterceptors } from './common/interceptors';
 
+// 엑시오스 초기화
 function createInstance() {
 	const instance = axios.create({
 		// baseURL: 'http://localhost:3000/',
@@ -12,12 +13,20 @@ function createInstance() {
 
 const instance = createInstance();
 
+// 회원가입 API
 export function registerUser(userData) {
 	// const url = 'http://localhost:3000/signup';
 	// return axios.post(url, userData);
 	return instance.post('signup', userData);
 }
 
+// 로그인 API
 export function loginUser(userData) {
 	return instance.post('login', userData);
+}
+
+// 학습노트 데이터 조회 API
+
+export function fetchPost() {
+	return instance.get('posts');
 }
--- vue-til/src/views/MainPage.vue
+++ vue-til/src/views/MainPage.vue
@@ -2,12 +2,29 @@
 	<div>
 		<div class="main list-container contents">
 			<h1 class="page-header">Today I Learned</h1>
+			<ul>
+				<li v-for="postItem in postItems" :key="postItem._id">
+					{{ postItem.title }}
+				</li>
+			</ul>
 		</div>
 	</div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { fetchPost } from '@/api';
+import { onBeforeMount, ref } from 'vue';
+
+const postItems = ref([]);
+
+onBeforeMount(() => {
+	const fetchData = async () => {
+		const { data } = await fetchPost();
+		console.log('data', data);
+		postItems.value = data;
+	};
+	fetchData();
+});
 </script>
 
 <style></style>
